2aac8ba7c3a9d588d493910234da5100897d6769
Pasha Okuneu <p.okuneu@godeltech.com>
2024-10-01 13:57:18 +0200

    Adjust config for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,14 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tmajs-demo',
-  plugins: [
-    // Allows using React dev server along with building a React application with Vite.
-    // https://npmjs.com/package/@vitejs/plugin-react-swc
-    react(),
-    // Allows using the compilerOptions.paths property in tsconfig.json.
-    // https://www.npmjs.com/package/vite-tsconfig-paths
-    tsconfigPaths(),
-    // Allows using self-signed certificates to run the dev server using HTTPS.
-    // https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
-    basicSsl(),
-  ],
-  publicDir: './public',
-  server: {
-    // Exposes your dev server and makes it accessible for the devices in the same network.
-    host: true,
+  plugins: [react(), tsconfigPaths(), basicSsl()],
+  build: {
+    outDir: 'dist',
+    copyPublicDir: true,
   },
+  base: './',
+  envDir: 'root',
+  publicDir: './public',
+  appType: 'spa'
 });
 
